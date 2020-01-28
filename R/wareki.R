36f280df587234d13700ddd846be5e7380649d4e
zakkiiii <zakkiiii@outlook.jp>
2020-01-28 16:51:23 +0900

    be compatible with charactor vector with multicomponent.

--- R/wareki.R
+++ R/wareki.R
@@ -1,7 +1,7 @@
 #' Convert Japanese Calendar, Gen-gou, into Westerm Year in sentences.
 #'
 #'
-#' @param document Text Data including Japanese Calendar. Must be Character.
+#' @param document Text Data including Japanese Calendar. Must be Character vector.
 #' @param dummyText If something Trouble with the Output, the dummyText, a Piece of Text temporarily added to document, may be the cause. If needed, instead of the default dummyText, you may use your original dummyText, which is recommended to be a reasonably long String without using Numeric.
 #' @param loop Whether the Process applies again and again to a Part once converted. Default FALSE is strongly recommended.
 #'
@@ -13,51 +13,54 @@
 #' @importFrom utils read.csv
 #' @importFrom utils data
 #'
-#' @return Character. Document Converted.
+#' @return Character Vector. Document Converted.
 #' @export
 #'
 wareki <- function(document, dummyText = NA, loop = FALSE) {
-  if (is.na(dummyText)) {
-    dummyText <- "dummymojiretsudesuyo_saigonikeshimasu"
-  }
-  for (gengou in gengouList[, "gengou"]) {
-    gengouFirstYear <- gengouList[gengouList$gengou == gengou, "year"]
-    detectYear <- str_c("(", gengou, ".\u5e74|", gengou, "..\u5e74|", gengou, "...\u5e74)")
-    while (str_detect(document, pattern = detectYear)) {
-      startYear <- regexpr(detectYear, document, useBytes = F)
-      endYear <- startYear + attr(startYear, "match.length") - 1
-      myYear <- substr(document, startYear, endYear)
-      if (str_sub(myYear, start = 3, end = 4) == gengou) {
-        startYear <- regexpr(detectYear, document) + 2
-        myYear <- substr(document, startYear, endYear)
-      }
-      if (str_sub(myYear, start = 4, end = 5) == gengou) {
-        replacementTx <- substr(document, startYear + 2, endYear)
-        replacementTx <- str_c(gengou, dummyText, replacementTx)
-        document <- gsub(myYear, replacementTx, document)
-      } else {
-        numericDetect <- str_c("(", gengou, "|\u5e74)")
-        waYear <- gsub(numericDetect, "", myYear)
-        if (waYear == "\u5143") {
-          waYear <- as.numeric(1)
+  document_vector <- c()
+  for(document_x in document) {
+    if (is.na(dummyText)) {
+      dummyText <- "dummymojiretsudesuyo_saigonikeshimasu"
+    }
+    for (gengou in gengouList[, "gengou"]) {
+      gengouFirstYear <- gengouList[gengouList$gengou == gengou, "year"]
+      detectYear <- str_c("(", gengou, ".\u5e74|", gengou, "..\u5e74|", gengou, "...\u5e74)")
+      while (str_detect(document_x, pattern = detectYear)) {
+        startYear <- regexpr(detectYear, document_x, useBytes = F)
+        endYear <- startYear + attr(startYear, "match.length") - 1
+        myYear <- substr(document_x, startYear, endYear)
+        if (str_sub(myYear, start = 3, end = 4) == gengou) {
+          startYear <- regexpr(detectYear, document_x) + 2
+          myYear <- substr(document_x, startYear, endYear)
         }
-        romaNumeric <- suppressWarnings(as.numeric(waYear))
-        if (is.na(romaNumeric)) {
-          excluded <- as.character(waYear)
-          temp <- str_c(gengou, dummyText, excluded, "\u5e74")
-          document <- gsub(myYear, temp, document) %>% as.character()
-        }else{
-          seireki <- gengouFirstYear + romaNumeric - 1
-          if (loop == FALSE) {
-            convertedYear <- str_c(dummyText, seireki, "\u5e74")
-          } else {
-            convertedYear <- str_c(seireki, "\u5e74")
+        if (str_sub(myYear, start = 4, end = 5) == gengou) {
+          replacementTx <- substr(document_x, startYear + 2, endYear)
+          replacementTx <- str_c(gengou, dummyText, replacementTx)
+          document_x <- gsub(myYear, replacementTx, document_x)
+        } else {
+          numericDetect <- str_c("(", gengou, "|\u5e74)")
+          waYear <- gsub(numericDetect, "", myYear)
+          if (waYear == "\u5143") {
+            waYear <- as.numeric(1)
           }
-          document <- gsub(myYear, convertedYear, document) %>% as.character()
+          romaNumeric <- suppressWarnings(as.numeric(waYear))
+          if (is.na(romaNumeric)) {
+            excluded <- as.character(waYear)
+            temp <- str_c(gengou, dummyText, excluded, "\u5e74")
+            document_x <- gsub(myYear, temp, document_x) %>% as.character()
+          }else{
+            seireki <- gengouFirstYear + romaNumeric - 1
+            if (loop == FALSE) {
+              convertedYear <- str_c(dummyText, seireki, "\u5e74")
+            } else {
+              convertedYear <- str_c(seireki, "\u5e74")
+            }
+            document_x <- gsub(myYear, convertedYear, document_x) %>% as.character()
+          }
         }
       }
     }
+    document_vector <- c(document_vector, gsub(as.character(dummyText), "", document_x))
   }
-  document <- gsub(as.character(dummyText), "", document)
-  document
+  document_vector
 }
--- R/wareki2.R
+++ R/wareki2.R
@@ -1,7 +1,7 @@
 #' Convert Japanese Calendar, Gen-gou, into Westerm Year in Sentences.
 #'
 #'
-#' @param document Text Data including Japanese Calendar. Must be Character.
+#' @param document Text Data including Japanese Calendar. Must be Character vector.
 #' @param dummyText If something Trouble with the Output, the dummyText, a Piece of Text temporarily  added to document, may be the cause. If needed, instead of the default dummyText, you may use your original dummyText, which is recommended to be a reasonably long String without using numeric.
 #' @param loop Whether the Process applies again and again to a Part once converted. Default FALSE is strongly recommended.
 #'
@@ -14,61 +14,64 @@
 #' @importFrom utils data
 #'
 #'
-#' @return Character. Document Converted.
+#' @return Character Vector. Document Converted.
 #' @export
 #'
 wareki2 <- function(document, dummyText = NA, loop = FALSE) {
-  if (is.na(dummyText)) {
-    dummyText <- "dummymojiretsudesuyo_saigonikeshimasu"
-  }
-  for (gengou in gengouList[, "gengou"]) {
-    gengouFirstYear <- gengouList[gengouList$gengo == gengou, "year"] %>% as.numeric()
-    detectYear <- str_c("(", gengou, ".", "|", gengou, "..", "|", gengou, "...", ")")
-    while (str_detect(document, pattern = detectYear)) {
-      startYear <- regexpr(detectYear, document, useBytes = F)
-      endYear <- startYear + attr(startYear, "match.length") - 1
-      myYear <- substr(document, startYear, endYear)
-      numericDetect <- str_c("(", gengou, ")")
-      waYear <- gsub(numericDetect, "", myYear)
-      waYear1 <- waYear
-      waYearX1 <- waYear
-      if (waYear1 == "\u5143") {
-        waYear1 <- as.numeric(1)
-      }
-      romaNumeric <- suppressWarnings(as.numeric(waYear1))
-      beforeText <- str_c(gengou, waYearX1)
-      if (is.na(romaNumeric)) {
-        waYear2 <- str_sub(waYear, end = -2)
-        waYearX2 <- waYear2
-        if (waYear2 == "\u5143") {
-          waYear2 <- as.numeric(1)
+  document_vector <- c()
+  for(document_x in document) {
+    if (is.na(dummyText)) {
+      dummyText <- "dummymojiretsudesuyo_saigonikeshimasu"
+    }
+    for (gengou in gengouList[, "gengou"]) {
+      gengouFirstYear <- gengouList[gengouList$gengo == gengou, "year"] %>% as.numeric()
+      detectYear <- str_c("(", gengou, ".", "|", gengou, "..", "|", gengou, "...", ")")
+      while (str_detect(document_x, pattern = detectYear)) {
+        startYear <- regexpr(detectYear, document_x, useBytes = F)
+        endYear <- startYear + attr(startYear, "match.length") - 1
+        myYear <- substr(document_x, startYear, endYear)
+        numericDetect <- str_c("(", gengou, ")")
+        waYear <- gsub(numericDetect, "", myYear)
+        waYear1 <- waYear
+        waYearX1 <- waYear
+        if (waYear1 == "\u5143") {
+          waYear1 <- as.numeric(1)
         }
-        romaNumeric <- suppressWarnings(as.numeric(waYear2))
-        beforeText <- str_c(gengou, waYearX2)
-      }
-      if (is.na(romaNumeric)) {
-        waYear3 <- str_sub(waYear, end = -3)
-        waYearX3 <- waYear3
-        if (waYear3 == "\u5143") {
-          waYear3 <- as.numeric(1)
+        romaNumeric <- suppressWarnings(as.numeric(waYear1))
+        beforeText <- str_c(gengou, waYearX1)
+        if (is.na(romaNumeric)) {
+          waYear2 <- str_sub(waYear, end = -2)
+          waYearX2 <- waYear2
+          if (waYear2 == "\u5143") {
+            waYear2 <- as.numeric(1)
+          }
+          romaNumeric <- suppressWarnings(as.numeric(waYear2))
+          beforeText <- str_c(gengou, waYearX2)
         }
-        romaNumeric <- suppressWarnings(as.numeric(waYear3))
-        beforeText <- str_c(gengou, waYearX3)
-      }
-      if (is.na(romaNumeric)) {
-        excluded <- as.character(waYear)
-        temp <- str_c(substr(gengou, 1, 1), dummyText, substr(gengou, 2, 2), excluded)
-        document <- gsub(myYear, temp, document) %>% as.character()
-      } else {
-        seireki <- as.numeric(gengouFirstYear) + as.numeric(romaNumeric) - 1
-        seireki %<>% as.character()
-        if (loop == FALSE) {
-          seireki <- str_c(dummyText, seireki, dummyText)
+        if (is.na(romaNumeric)) {
+          waYear3 <- str_sub(waYear, end = -3)
+          waYearX3 <- waYear3
+          if (waYear3 == "\u5143") {
+            waYear3 <- as.numeric(1)
+          }
+          romaNumeric <- suppressWarnings(as.numeric(waYear3))
+          beforeText <- str_c(gengou, waYearX3)
         }
-        document <- gsub(beforeText, seireki, document) %>% as.character()
+        if (is.na(romaNumeric)) {
+          excluded <- as.character(waYear)
+          temp <- str_c(substr(gengou, 1, 1), dummyText, substr(gengou, 2, 2), excluded)
+          document_x <- gsub(myYear, temp, document_x) %>% as.character()
+        } else {
+          seireki <- as.numeric(gengouFirstYear) + as.numeric(romaNumeric) - 1
+          seireki %<>% as.character()
+          if (loop == FALSE) {
+            seireki <- str_c(dummyText, seireki, dummyText)
+          }
+          document_x <- gsub(beforeText, seireki, document_x) %>% as.character()
+        }
       }
     }
+    document_vector <- c(document_vector, gsub(as.character(dummyText), "", document_x))
   }
-  document <- gsub(as.character(dummyText), "", document)
-  document
+  document_vector
 }
